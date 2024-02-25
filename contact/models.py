8bd69487c24be24e1a81244c26ecdf004394f8ad
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-02-25 17:16:41 -0300

    Create Contacts model and migrating it

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+# This module will be used to store the date and time when a contact is created.
+from django.utils import timezone
+
 # Create your models here.
+"""
+My model is a representation of the data that I want to store in my database.
+
+I will create a model for a contact, which will have the following fields:
+id (primary key - automatically created by Django),
+first_name(String), 
+last_name(String), 
+phone (String), 
+email (Email), 
+created_at (Date), 
+description (Text)
+category (Foreign Key), 
+show (Boolean), 
+owner (Foreign Key), 
+picture (image)
+"""
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    created_at = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
