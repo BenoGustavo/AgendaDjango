2a28eb09fc33d56e2b12df5fce67e0046cef20f4
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-02-28 19:38:02 -0300

    passing all the context to the html and showing the first name on the page

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 
 def index(request):
+    # This will get all the contacts from the database.
+    contacts = Contact.objects.all()
+
+    # This is the context with all the contacts data will be passed to the template.
+    context = {
+        "contacts": contacts,
+    }
+
     return render(
         request,
         "contact/index.html",
+        context,
     )
