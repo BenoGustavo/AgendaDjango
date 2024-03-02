8b3b4b0673eeda9d338446056b7b19ab4beabb44
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-03-02 16:14:48 -0300

    adding the table_headers to the context to use in the html

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -4,11 +4,14 @@ from contact.models import Contact
 
 def index(request):
     # This will get all the contacts from the database.
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by("-id")
+
+    table_headers = ["ID", "First name", "Last name", "Phone", "Email"]
 
     # This is the context with all the contacts data will be passed to the template.
     context = {
         "contacts": contacts,
+        "table_headers": table_headers,
     }
 
     return render(
