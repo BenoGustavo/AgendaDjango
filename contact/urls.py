50e039361078e7a311974d3c0dcc77d9792a3138
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-03-02 19:53:14 -0300

    creating the url of the single contact page also create the view with the context

--- contact/urls.py
+++ contact/urls.py
@@ -21,5 +21,6 @@ from contact import views
 app_name = "contact"
 
 urlpatterns = [
+    path("<int:contact_id>/", views.single_contact, name="single_contact"),
     path("", views.index, name="index"),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 
@@ -17,5 +17,21 @@ def index(request):
     return render(
         request,
         "contact/index.html",
+        context,
+    )
+
+
+def single_contact(request, contact_id: int):
+    # This will get the contact from the database and if it doesn't exist returns a 404 error.
+    contact = get_object_or_404(Contact.objects.filter(pk=contact_id), show=True)
+
+    # This is the context with the contact data will be passed to the template.
+    context = {
+        "contact": contact,
+    }
+
+    return render(
+        request,
+        "contact/single_contact.html",
         context,
     )
