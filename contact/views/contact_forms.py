ff2635089269bae314e0ea4f73438f0e5c1f75e9
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-03-06 18:03:51 -0300

    now it renders differently for get and post

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from contact.models import Contact
-from django.db.models import Q
-
-from contact.views.utils import paginatorWrap
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 
 def create(request):
+    # This will be the view for the contact creation form, if the user is only entering the page it won't trigger.
+    if request.method == "POST":
+        # This is the context with all the contacts data will be passed to the template.
+        context = {"form": ContactForm(request.POST)}
+
+        return render(
+            request,
+            "contact/create.html",
+            context,
+        )
+
     # This is the context with all the contacts data will be passed to the template.
-    context = {}
+    context = {
+        "form": ContactForm(),
+    }
 
     return render(
         request,
